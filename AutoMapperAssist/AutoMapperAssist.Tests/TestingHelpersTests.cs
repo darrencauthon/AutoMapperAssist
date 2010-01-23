6a3efd161f674758c50ea21329638556bed1513f
darrencauthon <darren@cauthon.com>
2010-01-23 16:18:15 -0600

    Create static extension method for testing.

---
+++ AutoMapperAssist/AutoMapperAssist.Tests/TestingHelpersTests.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutoMapperAssist.Tests
+{
+    [TestClass]
+    public class TestingHelpersTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof (AutoMapperConfigurationException))]
+        public void AssertConfigurationIsValid_PassedInvalidMapper_ThrowsException()
+        {
+            (new InvalidObjectMapper()).AssertConfigurationIsValid();
+        }
+
+        [TestMethod]
+        public void AssertConfigurationIsValid_PassedValidMapper_DoesNotThrowException()
+        {
+            (new ValidObjectMapper()).AssertConfigurationIsValid();
+        }
+    }
+
+    public class ValidObjectMapper : ObjectMapper<IAmLikeAnApple, IAmAlsoLikeAnApple>
+    {
+    }
+
+    public class InvalidObjectMapper : ObjectMapper<IAmLikeAnApple, IAmLikeAnOrange>
+    {
+    }
+
+    public class IAmLikeAnApple
+    {
+        public string Apple { get; set; }
+    }
+
+    public class IAmAlsoLikeAnApple
+    {
+        public string Apple { get; set; }
+    }
+
+    public class IAmLikeAnOrange
+    {
+        public string Orange { get; set; }
+    }
+}
---
+++ AutoMapperAssist/AutoMapperAssist/TestingHelpers.cs
@@ -0,0 +1,12 @@
+﻿namespace AutoMapperAssist
+{
+    public static class TestingHelpers
+    {
+        public static void AssertConfigurationIsValid(this IMapToDefine mapToDefine)
+        {
+            var configuration = ConfigurationHelper.CreateDefaultConfiguration();
+            mapToDefine.DefineMap(configuration);
+            configuration.AssertConfigurationIsValid();
+        }
+    }
+}
