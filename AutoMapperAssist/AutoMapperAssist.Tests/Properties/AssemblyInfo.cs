da7fc566f85ca6432e3a89ea8a9d6bb516ab89a3
darrencauthon <darren@cauthon.com>
2010-02-17 21:03:36 -0600

    Update assembly information.

--- AutoMapperAssist/AutoMapperAssist.Tests/Properties/AssemblyInfo.cs
+++ AutoMapperAssist/AutoMapperAssist.Tests/Properties/AssemblyInfo.cs
@@ -1,25 +1,27 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("AutoMapperAssist.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("DEG")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AutoMapperAssist.Tests")]
-[assembly: AssemblyCopyright("Copyright © DEG 2010")]
+[assembly: AssemblyCopyright("Copyright � Darren Cauthon 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM componenets.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("96eb0646-7812-43fd-8f39-b77ba41ce25a")]
 
 // Version information for an assembly consists of the following four values:
@@ -31,5 +33,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
